3709a9a83e151420933b3d26baff85f22b06fa68
YuboLin1 <yubol@nycstudents.net>
2025-05-13 11:45:15 -0400

    Update partA.java

--- Q2/partA.java
+++ Q2/partA.java
@@ -1,1 +1,25 @@
+public class SignedText{
+    private String name;
+    private String lname;
+    public SignedText(String n, String ln){
+        name = n;
+        lname = ln;
+    }
+    public String getSignature(){
+        if(name.length() == 0){
+            return lname;
+        }
+        return name.substring(0,1) + "-" + lname;
+    }
+    public String addSignature(String str){
+        if(str.indexOf(getSignature()) == -1){
+            return str + getSignature();
+        }
+        if(str.indexOf(getSignature()) == 0){
+            return str.substring(getSignature().length()) + getSignature();
+        }
+        return str;
+    }
 
+
+}
