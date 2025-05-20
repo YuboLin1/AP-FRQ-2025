e3694aa345d9ac3429ff5e911f777aca71aae6dd
YuboLin1 <yubol@nycstudents.net>
2025-05-20 11:49:37 -0400

    Update partB.java

--- Q3/partB.java
+++ Q3/partB.java
@@ -1,1 +1,14 @@
-
+public ArrayList<Match> buildMatches(){
+    ArrayList<Match> result = new ArrayList<Match>;
+    if(competitorList.size() % 2 == 1){
+        for(int i = 1; i < arr.length/2+1; i++){
+            result.add(Match(competitorList[i], competitorList(competitorList.size() - i)));
+        }
+    }
+    else {
+        for(int i = 0; i < arr.length/2; i++){
+            result.add(Match(competitorList[i], competitorList(competitorList.size() - i)));
+       }
+    }
+    return result;
+}
