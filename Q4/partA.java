bae43f17da244cc5b564f99b147e089e3bd63ef5
YuboLin1 <yubol@nycstudents.net>
2025-05-21 11:14:56 -0400

    Update partA.java

--- Q4/partA.java
+++ Q4/partA.java
@@ -1,8 +1,8 @@
 public SumOrSameGame(int numRows, int numCols)
 {
-    grid = new int[numRows][numCols];
+    puzzle = new int[numRows][numCols];
 
-    for(int r = 0; r < grid.length; r++)
-        for(int c = 0; c < grid[0].length; c++)
-            grid[r][c] = (int) (Math.random() * 9) + 1;
+    for(int r = 0; r < puzzle.length; r++)
+        for(int c = 0; c < puzzle[0].length; c++)
+            puzzle[r][c] = (int) (Math.random() * 9) + 1;
 }
